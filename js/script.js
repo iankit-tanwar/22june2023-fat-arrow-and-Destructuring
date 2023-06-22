9f0bb34e3712a8d9fa442098332e428bea95e947
iankit-tanwar <tankitofficial01.gmail.com>
2023-06-22 18:46:49 +0530

    commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//1 define
+
+let myFunctionE = (a, b) => a + b;
+
+
+//funtion calling
+console.log(myFunctionE(33, 55));
+
+//js obbject
+
+let students = {
+    fName: 'Ankit',
+    lName: "Tanwar",
+    addres: 'delhi',
+    phno: '99999999'
+
+}
+
+//destructuring / excract
+
+let {fName,lName} = students;
+
+
+console.log(fName,lName) ;
+
+
